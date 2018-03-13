5b63fc3007c854f58978ab4d2980e9d1daa91898
Bridgette Kirk <bdaniellekirk@gmail.com>
2018-03-13 11:16:38 -0500

    Wrangling for successional affinity and seed size for table 1 on seed rain characteristics

---
+++ Wrangling/SA_wrangling.R
@@ -0,0 +1,54 @@
+# File started on 9 March 2018
+
+# This file will look at successional affinity and ecological attributes of the seed
+
+# RERUN based on updates to affinity of miconia affinis.
+
+
+# load libraries
+library(readxl); library(plyr); library(ggplot2); library(reshape2); library(base); library(dplyr); library(scales)
+
+# set wd
+setwd("~/M.S. Thesis/Data/GitHubProjects/Succession/Data/Raw")
+
+# Bring in data
+sa_seed <-read_excel("Seedsize_sa.xlsx", sheet=2, col_names=TRUE, na= "NA")
+
+# subset data to include only columns for successional affinity analysis
+
+sa_seed2 <- subset(sa_seed, select = c(3, 4, 5, 6, 13))
+
+sa_seed2$species <- as.factor(sa_seed2$species)
+sa_seed2$lifeform <- as.factor(sa_seed2$lifeform)
+sa_seed2$dispersal <- as.factor(sa_seed2$dispersal)
+sa_seed2$size_cat <- as.factor(sa_seed2$size_cat)
+sa_seed2$suc_affinity <- as.factor(sa_seed2$suc_affinity)
+str(sa_seed2)
+
+
+# remove rows where no successional affinity is available
+
+sa_seed3 <- filter(sa_seed2, suc_affinity != "NA")
+# 59 species classified to suc_affinity
+
+# Need to add abundance data from each treatment to determine richness.
+
+# bring in abundance file.
+abund <- read.csv("abund_summary_year_notrtsp_nw.csv")
+abund2 <- subset(abund, select = c(1:5))
+
+sa_abund <- left_join(sa_seed3, abund2, by = "species")
+summary(sa_abund)
+
+sa_abund2 <- melt(sa_abund, id.vars = c("species", "dispersal", "lifeform", "size_cat", "suc_affinity"), value=seednum)
+sa_abund3 <- filter(sa_abund2, value >= 1)
+
+# % of seeds within each successional category
+sa_abund4 <- ddply(sa_abund3, .(variable, suc_affinity), summarise, seednum=sum(value))
+str(sa_abund4)
+
+sa_abund5 <- sa_abund4 %>% group_by(variable) %>% summarise(trt_tot = sum(seednum)) %>% right_join(sa_abund4) %>% mutate(percent = percent(seednum/trt_tot))
+
+# number of species within each successional affinity
+sa_abund6 <- ddply(sa_abund3, .(variable, suc_affinity), summarise, richness = length(species))
+
---
+++ Wrangling/Seedsize_wrangling.R
@@ -0,0 +1,50 @@
+# This file was created on 9 March 18
+
+# This file will look at seed size and different trait attributes
+
+# load libraries
+library(readxl); library(plyr); library(ggplot2); library(reshape2); library(base); library(dplyr); library (scales)
+
+# set wd
+setwd("~/M.S. Thesis/Data/GitHubProjects/Succession/Data/Raw")
+
+# Bring in data
+size <-read_excel("Seedsize_sa.xlsx", sheet=2, col_names=TRUE, na= "NA")
+
+# subset data to include only columns for successional affinity analysis
+
+size2 <- subset(size, select = c(3, 4, 5, 6))
+
+size2$species <- as.factor(size2$species)
+size2$lifeform <- as.factor(size2$lifeform)
+size2$dispersal <- as.factor(size2$dispersal)
+size2$size_cat <- as.factor(size2$size_cat)
+str(size2)
+
+
+# remove rows where no size category is available
+
+size3 <- filter(size2, size_cat != "NA")
+# 91 species classified to suc_affinity
+
+# Need to add abundance data from each treatment to determine richness.
+
+# bring in abundance file.
+abund <- read.csv("abund_summary_year_notrtsp_nw.csv")
+abund2 <- subset(abund, select = c(1:5))
+
+size_abund <- left_join(size3, abund2, by = "species")
+summary(size_abund)
+
+size_abund2 <- melt(size_abund, id.vars = c("species", "dispersal", "lifeform", "size_cat"), value=seednum)
+size_abund3 <- filter(size_abund2, value >= 1)
+
+# % of seeds within each successional category
+size_abund4 <- ddply(size_abund3, .(variable, size_cat), summarise, seednum=sum(value))
+str(size_abund4)
+
+size_abund5 <- size_abund4 %>% group_by(variable) %>% summarise(trt_tot = sum(seednum)) %>% right_join(size_abund4) %>% mutate(percent = percent(seednum/trt_tot))
+
+# number of species within each successional affinity
+size_abund6 <- ddply(size_abund3, .(variable, size_cat), summarise, richness = length(species))
+
